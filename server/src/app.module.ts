eb062cb96afdeec111b29f2924f62f0c2919a6c2
serhii <kozenkovs366@gmail.com>
2026-01-29 11:04:13 +0200

    libs and settings for server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,25 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+        disableErrorMessages: false,
+        validationError: { target: false, value: false },
+        transform: true,
+        transformOptions: {
+          enableImplicitConversion: true,
+        },
+      }),
+    },
+  ],
 })
 export class AppModule {}
