5ce0792028324bfa05336b2dd13e924f5013b02b
serhii <kozenkovs366@gmail.com>
2026-02-15 15:12:46 +0200

    refactoring and fixing errors with db and all details

--- server/src/clothes/clothes.module.ts
+++ server/src/clothes/clothes.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClothesController } from './clothes.controller';
 import { ClothesService } from './clothes.service';
 import { Clothing } from './clothing.entity';
+import { AiModule } from '../ai/ai.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Clothing])],
+  imports: [TypeOrmModule.forFeature([Clothing]), AiModule],
   controllers: [ClothesController],
   providers: [ClothesService],
   exports: [ClothesService],
